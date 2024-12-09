861400c7285bf2bed93a53e4f3fb9bd711ffc502
junaidsj <junaidahmedsj4949@gmail.com>
2024-12-09 16:24:31 +0530

    initial commit

---
+++ second.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n, sum;
+	cout << "Enter n" << endl;
+	cin >> n;
+	sum = n * (n + 1) / 2;
+	cout << "the sum is" << sum << endl;
+	return 0;
+}
